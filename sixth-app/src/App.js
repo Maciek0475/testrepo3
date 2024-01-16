35be5f4d8ae1d839c7616b3428f59981e691f13f
Maciej Jurczak <mac.jurczak@o2.pl>
2024-01-16 16:13:33 +0100

    Create basic operations
    Doing stuff in server.js and fetching from app.js

--- sixth-app/src/App.js
+++ sixth-app/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+const handleGet = async() => {
+  const smh = {};
+  const requestOptions = {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(smh)
+  }
+  fetch("http://localhost:5000/", requestOptions)
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleGet}>Get</button>
     </div>
   );
 }
